f7a344d137c8a38c1d85228ffe792699fbf97af7
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-09-10 08:08:51 +0600

    add followers models

--- network/models.py
+++ network/models.py
@@ -12,3 +12,13 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}..."
+
+class Followers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+
+    class Meta:
+        unique_together = ('user', 'follower')
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.user.username}"
