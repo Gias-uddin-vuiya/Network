76d7a1a47eae0680366252a0d688cbd55c4d307d
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-09-13 10:19:51 +0600

    add edit features frontend part

---
+++ network/static/network/main.js
@@ -0,0 +1,40 @@
+
+const editContentBtn = document.querySelectorAll(".edit-content-btn")
+
+
+editContentBtn.forEach((btn) => {
+  btn.addEventListener('click', (evt) => {
+    console.log("edit button clicked");
+
+    // Find the post container
+    const post = evt.target.closest(".post"); 
+    console.log(post)
+    const content = post.querySelector(".post-content");
+
+    // Create textarea and set the value to current content
+    const textarea = document.createElement("textarea");
+    textarea.className = "post-content-edit form-control";
+    textarea.value = content.innerText;
+
+    // Replace the content with textarea
+    content.replaceWith(textarea);
+
+
+
+    // You can add a save button after editing
+    const saveBtn = document.createElement("button");
+    saveBtn.innerText = "Save";
+    saveBtn.className = "save-btn btn btn-primary";
+    textarea.insertAdjacentElement("afterend", saveBtn);
+
+    saveBtn.addEventListener("click", () => {
+      const newContent = document.createElement("p");
+      newContent.className = "post-content";
+      newContent.innerText = textarea.value;
+
+      // Replace textarea back with updated content
+      textarea.replaceWith(newContent);
+      saveBtn.remove();
+    });
+  });
+});
